bf5e1691fbb54f3f8729885d7a1fceb520555fff
horbmyk <67599881+horbmyk@users.noreply.github.com>
2021-08-18 20:51:28 +0300

    Added SetSizeGameBoard

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace CrossesAndZeros
+{
+    public class LevelController : MonoBehaviour
+    {
+        [SerializeField] private GameBoardController GameBoardController;
+        private void Start()
+        {
+            InitializationGameBoard(GameBoardController);
+        }
+
+        private void InitializationGameBoard(GameBoardController gameBoardController)
+        {
+            gameBoardController.SetSizeGameBoard(3);
+        }
+    }
+}

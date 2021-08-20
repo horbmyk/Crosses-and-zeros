92712b388388bff9cf3cbfeb7db598b318c57184
horbmyk <67599881+horbmyk@users.noreply.github.com>
2021-08-20 15:19:55 +0300

    Update ComputerSide.cs

--- Assets/Scripts/ComputerSide.cs
+++ Assets/Scripts/ComputerSide.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CrossesAndZeros
@@ -8,16 +9,53 @@ namespace CrossesAndZeros
     {
         public void Move(Cell[][] poolCells, out int indexColumn, out int indexRow)
         {
-            indexColumn = 0;
-            indexRow = 0;
+            indexColumn = 0;//
+            indexRow = 0;//
+            List<FreeCell> poolFreeCell = new List<FreeCell>();
 
             for (int i = 0; i < poolCells.Length; i++)
             {
                 for (int j = 0; j < poolCells[i].Length; j++)
                 {
-                    Debug.Log("row " + i + " col " + j + " = " + poolCells[i][j].SelectedValue);
 
+                    if (poolCells[i][j].SelectedValue != 0)
+                        continue;
+
+                    poolFreeCell.Add(new FreeCell(i, j));
                 }
+            }
+
+            if (poolFreeCell.Count > 0)
+            {
+                int randNum = UnityEngine.Random.Range(0, poolFreeCell.Count - 1);
+                indexColumn = poolFreeCell[randNum].ValueIndexCol;
+                indexRow = poolFreeCell[randNum].ValueIndexRow;
+            }
+            else
+            {
+                Debug.Log("else");
+            }
+
+        }
+
+        private class FreeCell
+        {
+            private int IndexRow;
+            public int ValueIndexRow
+            {
+                get { return IndexRow; }
+                set { IndexRow = value; }
+            }
+            private int IndexCol;
+            public int ValueIndexCol
+            {
+                get { return IndexCol; }
+                set { IndexCol = value; }
+            }
+            public FreeCell(int indexRow, int indexCol)
+            {
+                IndexRow = indexRow;
+                IndexCol = indexCol;
             }
         }
     }
